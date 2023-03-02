c209ed9b19de055528cd9ecc855207d335166040
promius <bodiachernii@gmail.com>
2023-03-02 14:42:47 +0200

    refactor

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,40 +6,49 @@ import {todosActions} from "../../redux/slices/todos.slice";
 
 import './form.scss'
 
-const Form:FC = () => {
+const Form: FC = () => {
   const [title, setTitle] = useState<string>('')
   const [description, setDescription] = useState<string>('')
+  const [send, setSend] = useState<boolean>(true)
   const dispatch = useAppDispatch()
-  const onSubmit = (e:FormEvent<HTMLFormElement>) => {
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    dispatch(todosActions.addTodo({title, description}))
-    setTitle('')
-    setDescription('')
+    if (title && description) {
+      dispatch(todosActions.addTodo({title, description}))
+      setTitle('')
+      setDescription('')
+      setSend(true)
+    }
+    else{
+      setSend(false)
+    }
+
   }
 
   return (
-    <form className='todosForm' onSubmit={(e)=>onSubmit(e)}>
+    <form className='todosForm' onSubmit={(e) => onSubmit(e)}>
       <div className="todosForm__item">
         <label htmlFor="title">Title: </label>
         <input
           type="text"
           id='title'
           name='title'
-          required={true}
           placeholder='Enter title'
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        {!send ? <p className='todosForm__item-error'>This field is empty</p> : null}
       </div>
       <div className="todosForm__item">
         <label htmlFor='description'>Description</label>
         <input
           type="text" id='description'
           name='description'
-          required={true}
           placeholder='Enter description'
           value={description}
           onChange={(e) => setDescription(e.target.value)}/>
+        {!send ? <p className='todosForm__item-error'>This field is empty</p> : null}
       </div>
       <button className="todosForm__submit" type='submit'>Create</button>
     </form>
